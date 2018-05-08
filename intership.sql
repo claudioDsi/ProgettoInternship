096f309403079ec3be09d77c875f44be9403f0ba
claudio <claudio.disipio94@gmail.com>
2018-05-08 12:44:56 +0200

    Modifiche DB

--- intership.sql
+++ intership.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Mag 02, 2018 alle 16:31
+-- Creato il: Mag 08, 2018 alle 12:42
 -- Versione del server: 10.1.29-MariaDB
 -- Versione PHP: 7.2.0
 
@@ -30,9 +30,14 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `azienda` (
   `IdAzienda` int(11) NOT NULL,
+  `Username` varchar(50) NOT NULL,
+  `Password` char(64) NOT NULL,
+  `Privilagi` int(11) NOT NULL,
   `Nome` varchar(250) NOT NULL,
+  `RagioneSociale` varchar(250) NOT NULL,
   `Indirizzo` varchar(250) NOT NULL,
-  `PartitaIva` varchar(250) NOT NULL,
+  `PartitaIva` varchar(11) NOT NULL,
+  `CodiceFiscale` varchar(16) NOT NULL,
   `NomeRappr` varchar(250) NOT NULL,
   `CognomeRappr` varchar(250) NOT NULL,
   `Responsabile` varchar(250) NOT NULL,
@@ -40,6 +45,34 @@ CREATE TABLE `azienda` (
   `Foro` varchar(250) NOT NULL,
   `Valutazione` float NOT NULL,
   `Convenzione` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `convezione`
+--
+
+CREATE TABLE `convezione` (
+  `IdConvenzione` int(11) NOT NULL,
+  `Dimensioni` varchar(250) NOT NULL,
+  `Tipo` varchar(250) NOT NULL,
+  `Filename` varchar(250) NOT NULL,
+  `CodAzienda` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `documento`
+--
+
+CREATE TABLE `documento` (
+  `IdDocumento` int(11) NOT NULL,
+  `Dimensioni` int(11) NOT NULL,
+  `Tipo` varchar(250) NOT NULL,
+  `Filename` varchar(250) NOT NULL,
+  `CodTirocinio` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -53,27 +86,10 @@ CREATE TABLE `richiesta` (
   `IdTiro` int(11) NOT NULL,
   `Status` varchar(20) NOT NULL,
   `Progetto` int(11) NOT NULL,
-  `Cfu` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `studente`
---
-
-CREATE TABLE `studente` (
-  `IdStudente` int(11) NOT NULL,
-  `Matricola` int(11) NOT NULL,
-  `Nome` varchar(250) NOT NULL,
-  `Cognome` varchar(250) NOT NULL,
-  `DataNasc` date NOT NULL,
-  `LuogoNasc` varchar(250) NOT NULL,
-  `Residenza` varchar(250) NOT NULL,
-  `CodiceFisc` varchar(16) NOT NULL,
-  `Telefono` varchar(20) NOT NULL,
-  `CorsoLaurea` varchar(250) NOT NULL,
-  `Handicap` tinyint(1) NOT NULL
+  `Cfu` int(11) NOT NULL,
+  `NomeTutor` varchar(50) NOT NULL,
+  `CognomeTutor` varchar(50) NOT NULL,
+  `EmailTutor` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -91,6 +107,7 @@ CREATE TABLE `tirocinio` (
   `Obiettivi` varchar(250) NOT NULL,
   `Modalità` varchar(250) NOT NULL,
   `Facilitazioni` varchar(250) NOT NULL,
+  `Settore` varchar(50) NOT NULL,
   `IdTutore` int(11) NOT NULL,
   `idAzienda` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -106,7 +123,34 @@ CREATE TABLE `tutore` (
   `Nome` varchar(250) NOT NULL,
   `Cognome` varchar(250) NOT NULL,
   `DataNasc` date NOT NULL,
-  `NumTirocini` int(11) NOT NULL
+  `NumTirocini` int(11) NOT NULL,
+  `Telefono` varchar(20) NOT NULL,
+  `CodAzienda` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `utente`
+--
+
+CREATE TABLE `utente` (
+  `IdUtente` int(11) NOT NULL,
+  `Username` varchar(50) NOT NULL,
+  `Password` char(64) NOT NULL,
+  `Privilegi` int(11) NOT NULL,
+  `Nome` varchar(250) NOT NULL,
+  `Cognome` varchar(250) NOT NULL,
+  `DataNasc` date NOT NULL,
+  `LuogoNasc` varchar(250) NOT NULL,
+  `Residenza` varchar(250) NOT NULL,
+  `CodiceFisc` varchar(16) NOT NULL,
+  `Telefono` varchar(20) NOT NULL,
+  `CorsoLaurea` varchar(250) NOT NULL,
+  `Handicap` tinyint(1) NOT NULL,
+  `Laurea` varchar(50) NOT NULL,
+  `Dottorato` varchar(50) NOT NULL,
+  `ScuolaSpec` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -120,16 +164,22 @@ ALTER TABLE `azienda`
   ADD PRIMARY KEY (`IdAzienda`);
 
 --
+-- Indici per le tabelle `convezione`
+--
+ALTER TABLE `convezione`
+  ADD PRIMARY KEY (`IdConvenzione`);
+
+--
+-- Indici per le tabelle `documento`
+--
+ALTER TABLE `documento`
+  ADD PRIMARY KEY (`IdDocumento`);
+
+--
 -- Indici per le tabelle `richiesta`
 --
 ALTER TABLE `richiesta`
   ADD PRIMARY KEY (`IdStud`,`IdTiro`);
-
---
--- Indici per le tabelle `studente`
---
-ALTER TABLE `studente`
-  ADD PRIMARY KEY (`IdStudente`);
 
 --
 -- Indici per le tabelle `tirocinio`
@@ -144,6 +194,12 @@ ALTER TABLE `tutore`
   ADD PRIMARY KEY (`IdTutore`);
 
 --
+-- Indici per le tabelle `utente`
+--
+ALTER TABLE `utente`
+  ADD PRIMARY KEY (`IdUtente`);
+
+--
 -- AUTO_INCREMENT per le tabelle scaricate
 --
 
@@ -154,10 +210,16 @@ ALTER TABLE `azienda`
   MODIFY `IdAzienda` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT per la tabella `studente`
+-- AUTO_INCREMENT per la tabella `convezione`
 --
-ALTER TABLE `studente`
-  MODIFY `IdStudente` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `convezione`
+  MODIFY `IdConvenzione` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `documento`
+--
+ALTER TABLE `documento`
+  MODIFY `IdDocumento` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT per la tabella `tirocinio`
@@ -170,6 +232,12 @@ ALTER TABLE `tirocinio`
 --
 ALTER TABLE `tutore`
   MODIFY `IdTutore` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `utente`
+--
+ALTER TABLE `utente`
+  MODIFY `IdUtente` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
