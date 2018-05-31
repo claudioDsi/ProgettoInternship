beba26fc239beeca0d96bd4076cce5789db389ec
claudio <claudio.disipio94@gmail.com>
2018-05-31 17:02:22 +0200

    Aggiunta template result

---
+++ WebApplication1/src/java/it/example/framework/result/DataModelFiller.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.example.framework.result;
+
+import java.util.Map;
+
+/**
+ *
+ * @author claudio
+ */
+public interface DataModelFiller {
+    public void fillDataModel(Map datamodel);
+}
