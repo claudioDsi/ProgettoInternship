ade2859fe9aa5679dd0c40030631a75fddf00d5e
Roberta <robbycapuano@gmail.com>
2018-06-22 17:31:56 +0200

    login homepage dom

--- TirocinioFacile/web/script/login.js
+++ TirocinioFacile/web/script/login.js
@@ -1,21 +1,22 @@
 //LOGIN FORM
 var x = document.getElementById("form_login");
+
 var createbutton1 = document.createElement('input');
 createbutton1.setAttribute("class", "w3-button w3-block w3-yellow w3-round");
 createbutton1.setAttribute("style", "width:50%; float:right;");
 createbutton1.setAttribute("type", "button")
 createbutton1.setAttribute("name", "button1")
-createbutton1.setAttribute("onclick", "show('log_az')");
-createbutton1.setAttribute("value", "Login Azienda");
+createbutton1.setAttribute("onclick", "show('log_stud')");
+createbutton1.setAttribute("value", "Login Studente");
 x.appendChild(createbutton1);
 
 var createbutton2 = document.createElement('input');
 createbutton2.setAttribute("class", "w3-button w3-block w3-red w3-round");
 createbutton2.setAttribute("style", "width:50%; float:right;");
 createbutton2.setAttribute("type", "button")
-createbutton2.setAttribute("name", "button1")
-createbutton2.setAttribute("onclick", "show('log_stud')");
-createbutton2.setAttribute("value", "Login Studente");
+createbutton2.setAttribute("name", "button2")
+createbutton2.setAttribute("onclick", "show('log_az')");
+createbutton2.setAttribute("value", "Login Azienda");
 x.appendChild(createbutton2);
 
 var createStudDiv = document.createElement('div');
@@ -32,81 +33,94 @@ createFormStud.setAttribute("method", "post");
 createFormStud.setAttribute("action", "login");
 y.appendChild(createFormStud);
 
-var loginStud = document.createElement('input');
-loginStud.setAttribute("type", "hidden");
-loginStud.setAttribute("name", "tid");
-loginStud.setAttribute("value", "");
-y.appendChild(loginStud);
+var loginInput = document.createElement('input');
+loginInput.setAttribute("type", "hidden");
+loginInput.setAttribute("name", "tid");
+loginInput.setAttribute("value", "");
+y.appendChild(loginInput);
 
-var paragraf = document.createElement('p');
-y.appendChild(paragraf)
+var line = document.createElement('br');
+y.appendChild(line);
 
 var setUsername = document.createElement('input');
 setUsername.setAttribute("class", "w3-input w3-animate-input w3-yellow");
 setUsername.setAttribute("type", "text");
 setUsername.setAttribute("name", "username");
 setUsername.setAttribute("value", "");
-setUsername.setAttribute("style", "width:60%");
+setUsername.setAttribute("style", "width:60%; margin-left:15px");
 y.appendChild(setUsername);
 
 var usernameLabel = document.createElement('label'); // Create Label for Name Field
-usernameLabel.innerHTML = "Username : "; // Set Field Labels
+usernameLabel.setAttribute("style", "margin-left:15px;")
+usernameLabel.innerHTML = "Username"; // Set Field Labels
 y.appendChild(usernameLabel);
 
-/*
-var x = document.getElementById("form_sample");
-var createform = document.createElement('form'); // Create New Element Form
-createform.setAttribute("action", ""); // Setting Action Attribute on Form
-createform.setAttribute("method", "post"); // Setting Method Attribute on Form
-x.appendChild(createform);
+var paragraf = document.createElement('p');
+y.appendChild(paragraf);
 
-var heading = document.createElement('h2'); // Heading of Form
-heading.innerHTML = "Contact Form ";
-createform.appendChild(heading);
+var setPassword = document.createElement('input');
+setPassword.setAttribute("class", "w3-input w3-animate-input w3-yellow");
+setPassword.setAttribute("type", "text");
+setPassword.setAttribute("name", "username");
+setPassword.setAttribute("value", "");
+setPassword.setAttribute("style", "width:60%; margin-left:15px");
+y.appendChild(setPassword);
 
-var line = document.createElement('hr'); // Giving Horizontal Row After Heading
-createform.appendChild(line);
+var passwordLabel = document.createElement('label'); // Create Label for Name Field
+passwordLabel.setAttribute("style", "margin-left:15px");
+passwordLabel.innerHTML = "Password"; // Set Field Labels
+y.appendChild(passwordLabel);
 
-var linebreak = document.createElement('br');
-createform.appendChild(linebreak);
+y.appendChild(paragraf);
 
-var namelabel = document.createElement('label'); // Create Label for Name Field
-namelabel.innerHTML = "Your Name : "; // Set Field Labels
-createform.appendChild(namelabel);
+var loginButton = document.createElement('input');
+loginButton.setAttribute("class", "w3-button w3-section w3-teal w3-ripple");
+loginButton.setAttribute("type", "submit");
+loginButton.setAttribute("style", "margin-left:15px;")
+loginButton.setAttribute("name", "login");
+loginButton.setAttribute("value", "Login")
+y.appendChild(loginButton);
 
-var inputelement = document.createElement('input'); // Create Input Field for Name
-inputelement.setAttribute("type", "text");
-inputelement.setAttribute("name", "dname");
-createform.appendChild(inputelement);
+var createAzDiv = document.createElement('div');
+createAzDiv.setAttribute("id", "log_az");
+createAzDiv.setAttribute("class", "w3-red w3-round");
+createAzDiv.setAttribute("style", "display:none;");
+x.appendChild(createAzDiv);
 
-var linebreak = document.createElement('br');
-createform.appendChild(linebreak);
+var z = document.getElementById("log_az");
 
-var emaillabel = document.createElement('label'); // Create Label for E-mail Field
-emaillabel.innerHTML = "Your Email : ";
-createform.appendChild(emaillabel);
+var createFormAz = document.createElement('form');
+createFormAz.setAttribute("class", "w3-container w3-card-4");
+createFormAz.setAttribute("method", "post");
+createFormAz.setAttribute("action", "login");
+z.appendChild(createFormAz);
 
-var emailelement = document.createElement('input'); // Create Input Field for E-mail
-emailelement.setAttribute("type", "text");
-emailelement.setAttribute("name", "demail");
-createform.appendChild(emailelement);
+z.appendChild(loginInput);
 
-var emailbreak = document.createElement('br');
-createform.appendChild(emailbreak);
+z.appendChild(line);
 
-var messagelabel = document.createElement('label'); // Append Textarea
-messagelabel.innerHTML = "Your Message : ";
-createform.appendChild(messagelabel);
+var setUsernameAz = document.createElement('input');
+setUsernameAz.setAttribute("class", "w3-input w3-animate-input w3-red");
+setUsernameAz.setAttribute("type", "text");
+setUsernameAz.setAttribute("name", "username");
+setUsernameAz.setAttribute("value", "");
+setUsernameAz.setAttribute("style", "width:60%; margin-left:15px");
+z.appendChild(setUsernameAz);
 
-var texareaelement = document.createElement('textarea');
-texareaelement.setAttribute("name", "dmessage");
-createform.appendChild(texareaelement);
+z.appendChild(usernameLabel);
 
-var messagebreak = document.createElement('br');
-createform.appendChild(messagebreak);
+z.appendChild(paragraf);
 
-var submitelement = document.createElement('input'); // Append Submit Button
-submitelement.setAttribute("type", "submit");
-submitelement.setAttribute("name", "dsubmit");
-submitelement.setAttribute("value", "Submit");
-createform.appendChild(submitelement);*/
+var setPasswordAz = document.createElement('input');
+setPasswordAz.setAttribute("class", "w3-input w3-animate-input w3-red");
+setPasswordAz.setAttribute("type", "text");
+setPasswordAz.setAttribute("name", "username");
+setPasswordAz.setAttribute("value", "");
+setPasswordAz.setAttribute("style", "width:60%; margin-left:15px");
+z.appendChild(setPasswordAz);
+
+z.appendChild(passwordLabel);
+
+z.appendChild(paragraf);
+
+z.appendChild(loginButton);
