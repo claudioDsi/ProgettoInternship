f1d0935b3cbf2cc1f91526334442734aafa48d66
Roberta <robbycapuano@gmail.com>
2018-06-25 16:01:43 +0200

    porc...ahahahahah

---
+++ TirocinioFacile/web/script/showButton.js
@@ -0,0 +1,8 @@
+var x = document.getElementById("button1");
+var y = document.getElementById("button2");
+var z = document.getElementById("ulogin");
+var w = document.getElementById("labelLogin");
+x.style.display = "block";
+y.style.display = "block";
+z.style.display = "block";
+w.style.display = "block";
