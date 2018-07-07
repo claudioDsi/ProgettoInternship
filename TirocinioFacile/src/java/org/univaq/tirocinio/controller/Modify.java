2813bf91eaa5c32789e2fe3738b4f0a72be0f990
Vincenzo Battisti <vincenzo.battisti@gmail.com>
2018-07-07 11:16:36 +0200

    modifica update utente

--- TirocinioFacile/src/java/org/univaq/tirocinio/controller/Modify.java
+++ TirocinioFacile/src/java/org/univaq/tirocinio/controller/Modify.java
@@ -52,7 +52,7 @@ public class Modify extends InternshipDBController {
         }
     }
     
-    private void action_modify(HttpServletRequest request, HttpServletResponse response,int userid, String usertype) throws IOException, ServletException, TemplateManagerException, ParseException, NoSuchAlgorithmException {
+    private void action_modify(HttpServletRequest request, HttpServletResponse response, int userid, String usertype) throws IOException, ServletException, TemplateManagerException, ParseException, NoSuchAlgorithmException {
         try {
             TemplateResult res = new TemplateResult(getServletContext());
             //si controlla che l'username inserito o è quello vecchio o è uno nuovo ma non già scelto da altri utenti
@@ -90,93 +90,20 @@ public class Modify extends InternshipDBController {
             }
             if(!no_update){
                 if(usertype.equals("stud") || usertype.equals("admin")){
-                    Utente u = ((InternShipDataLayer)request.getAttribute("datalayer")).creaStudente();
-                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-                    String date_in_string = request.getParameter("datanasc");
-                    Date date = sdf.parse(date_in_string);              
-                    /*
-                    List<String> arr=new ArrayList<String>();     
-
-
-                    if(!SecurityLayer.addSlashes(request.getParameter("nome")).equals("")){
-                        arr.add("Nome");
-                    }                
-                      if(!SecurityLayer.addSlashes(request.getParameter("cognome")).equals("")){
-                        arr.add("Cognome");                }
-                      if(!SecurityLayer.addSlashes(request.getParameter("luogonasc")).equals("")){
-                        arr.add("LuogoNasc");
-                    }
-                      if(!SecurityLayer.addSlashes(request.getParameter("datanasc")).equals("")){
-                        arr.add("DataNasc");
-                    }
-                      if(!SecurityLayer.addSlashes(request.getParameter("codfisc")).equals("")){
-                        arr.add("CodiceFisc");
-                    }
-                      if(!SecurityLayer.addSlashes(request.getParameter("residenza")).equals("")){
-                        arr.add("Residenza");
-                    }
-                      if(!SecurityLayer.addSlashes(request.getParameter("telefono")).equals("")){
-                        arr.add("Telefono");
-                    }                  
-                      if(!SecurityLayer.addSlashes(request.getParameter("sesso")).equals("")){
-                        arr.add("Sesso");
-                    }                
-                      if(!SecurityLayer.addSlashes(request.getParameter("email")).equals("")){
-                        arr.add("EmailUtente");
-                    }
-                       if(!SecurityLayer.addSlashes(request.getParameter("handicap")).equals("")){
-                        arr.add("Handicap");
-                    }                 
-                      if(!SecurityLayer.addSlashes(request.getParameter("laurea")).equals("")){
-                        arr.add("CorsoLaurea");
-                    }
-                      if(!SecurityLayer.addSlashes(request.getParameter("dottorato")).equals("")){
-                        arr.add("Dottorato");
-                    }
-                        if(!SecurityLayer.addSlashes(request.getParameter("specializzazione")).equals("")){
-                        arr.add("ScuolaSpec");
-                    }                 
-                    if(!SecurityLayer.addSlashes(request.getParameter("cdl")).equals("")){
-                        arr.add("Laurea");
-                    }               
-                    String[] campi =new String[arr.size()];
-                    arr.toArray(campi);               
-                    for(int i=0; i<campi.length;i++){
-                        System.out.println("campo "+campi[i]);
-                    }
-                    System.out.println(request.getParameter("handicap"));
-                    */
-                    u.setNome(SecurityLayer.addSlashes(request.getParameter("nome")));                
-                    u.setCognome(SecurityLayer.addSlashes(request.getParameter("cognome")));
-                    u.setDataNasc(date);                 
-                    u.setLuogoNasc(SecurityLayer.addSlashes(request.getParameter("luogonasc")));                               
+                    Utente u = ((InternShipDataLayer)request.getAttribute("datalayer")).getInfoUtente(userid);                             
                     u.setResidenza(SecurityLayer.addSlashes(request.getParameter("residenza")));
-                    u.setCodFisc(SecurityLayer.addSlashes(request.getParameter("codfisc")));
                     u.setTelefono(SecurityLayer.addSlashes(request.getParameter("telefono")));
-
-                    u.setSesso(SecurityLayer.addSlashes(request.getParameter("sesso")));
                     u.setCdl(SecurityLayer.addSlashes(request.getParameter("cdl")));
                     u.setHandicap(Boolean.valueOf(request.getParameter("handicap")));
                     u.setLaurea(SecurityLayer.addSlashes(request.getParameter("laurea")));
                     u.setDottorato(SecurityLayer.addSlashes(request.getParameter("dottorato")));
                     u.setSpecializzazione(SecurityLayer.addSlashes(request.getParameter("specializzazione")));
                     u.setEmailUtente(SecurityLayer.addSlashes(request.getParameter("email")));
-                    u.setIdUtente(userid);
                     u.setUsername(SecurityLayer.addSlashes(request.getParameter("username")));
                     String password = SecurityLayer.addSlashes(request.getParameter("password"));
                     u.setPassword(SecurityLayer.securePassword(password));
-                   ((InternShipDataLayer)request.getAttribute("datalayer")).storeStudente(u);
-
-                   //query dinamica da verificare
-
-                   //String up=((InternShipDataLayer)request.getAttribute("datalayer")).creaQueryUpdate(campi, "Utente", "IdUtente");
-
-
-                   //System.out.println(up);
-
-                  //((InternShipDataLayer)request.getAttribute("datalayer")).modificaUtente(up,u);
-
-                   action_activate(request, response, userid, usertype);
+                    ((InternShipDataLayer)request.getAttribute("datalayer")).storeStudente(u);
+                    action_activate(request, response, userid, usertype);
                 }else if(usertype.equals("comp")){
                     Azienda a = ((InternShipDataLayer)request.getAttribute("datalayer")).creaAzienda();
                     a.setNomeAzienda(SecurityLayer.addSlashes(request.getParameter("nome")));
@@ -243,30 +170,38 @@ public class Modify extends InternshipDBController {
     @Override
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
         throws ServletException { 
-            HttpSession s = SecurityLayer.checkSession(request);
-            int userid = (int) s.getAttribute("userid");
-            String usertype = (String) s.getAttribute("type");
             try{
-                if(request.getParameter("update")!=null){
-                    //se l'azione di modifica è stata inviata, la eseguo
-                    action_modify(request, response, userid, usertype);
-                }else{
-                    int uid = SecurityLayer.checkNumeric(request.getParameter("uid"));
-                    String utype = request.getParameter("utype");
-                    if(uid == userid){
-                        //se l'utente che si vuole modificare è lo stesso di quello in sessione, posso modificare
-                        if(utype.equals("stud") || utype.equals("admin")){
-                            request.setAttribute("page_title", "Modifica Utente");
-                            request.setAttribute("Session", s);
-                            action_default(request, response, utype, userid);
-                        }else if(utype.equals("comp")){
-                            request.setAttribute("page_title", "Modifica Azienda");
-                            request.setAttribute("Session", s);
-                            action_default(request, response, utype, userid);
-                        }
+                HttpSession s = SecurityLayer.checkSession(request);
+                if(s!=null){
+                    int userid = (int) s.getAttribute("userid");
+                    String usertype = (String) s.getAttribute("type");            
+                    if(request.getParameter("update")!=null){
+                        //se l'azione di modifica è stata inviata, la eseguo
+                        action_modify(request, response, userid, usertype);
                     }else{
-                        response.sendRedirect("profile?uid="+uid+"&utype="+utype);
+                        if(request.getParameter("uid")!=null){
+                            int uid = SecurityLayer.checkNumeric(request.getParameter("uid"));
+                            String utype = request.getParameter("utype");
+                            if(uid == userid){
+                                //se l'utente che si vuole modificare è lo stesso di quello in sessione, posso modificare
+                                if(utype.equals("stud") || utype.equals("admin")){
+                                    request.setAttribute("page_title", "Modifica Utente");
+                                    request.setAttribute("Session", s);
+                                    action_default(request, response, utype, userid);
+                                }else if(utype.equals("comp")){
+                                    request.setAttribute("page_title", "Modifica Azienda");
+                                    request.setAttribute("Session", s);
+                                    action_default(request, response, utype, userid);
+                                }
+                            }else{
+                                response.sendRedirect("profile?uid="+userid+"&utype="+usertype);
+                            }
+                        }else{
+                            response.sendRedirect("profile?uid="+userid+"&utype="+usertype);
+                        }
                     }
+                }else{
+                    response.sendRedirect("home");
                 }
             }catch (IOException ex) {
                 request.setAttribute("exception", ex);
