e1e55db1eb4676f478be42d3aef5251af5b9c301
Vincenzo Battisti <vincenzo.battisti@gmail.com>
2018-07-09 15:59:13 +0200

    home funzionante

--- TirocinioFacile/src/java/org/univaq/tirocinio/controller/Homepage.java
+++ TirocinioFacile/src/java/org/univaq/tirocinio/controller/Homepage.java
@@ -6,13 +6,19 @@
 package org.univaq.tirocinio.controller;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import org.univaq.tirocinio.datamodel.Azienda;
 import org.univaq.tirocinio.datamodel.InternShipDataLayer;
+import org.univaq.tirocinio.datamodel.Richiesta;
 import org.univaq.tirocinio.datamodel.Tirocinio;
 import org.univaq.tirocinio.framework.data.DataLayerException;
 import org.univaq.tirocinio.framework.result.FailureResult;
@@ -27,9 +33,10 @@ import org.univaq.tirocinio.framework.security.SecurityLayer;
  */
 public class Homepage extends InternshipDBController {
     
-    private void action_show_home(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException, TemplateManagerException {
+    private void action_show_home_anonimo(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException, TemplateManagerException {
         List<Tirocinio> list = new ArrayList();
         try{
+            request.setAttribute("page_title", "Homepage");
             TemplateResult res = new TemplateResult(getServletContext());
             list = ((InternShipDataLayer)request.getAttribute("datalayer")).getListaTirocini();  
             request.setAttribute("strip_slashes", new SplitSlashesFmkExt());
@@ -45,6 +52,80 @@ public class Homepage extends InternshipDBController {
         }             
     }
     
+    private void action_show_home_admin(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException, TemplateManagerException {
+        try{
+            TemplateResult res = new TemplateResult(getServletContext());
+            request.setAttribute("page_title", "Gestione delle abilitazioni delle aziende");
+            HttpSession s = SecurityLayer.checkSession(request);
+            List<Azienda> lista_aziende = ((InternShipDataLayer)request.getAttribute("datalayer")).showListaAziende();
+            request.setAttribute("lista_aziende", lista_aziende);
+            request.setAttribute("Session", s);
+            request.setAttribute("strip_slashes", new SplitSlashesFmkExt());
+            res.activate("home.ftl.html", request, response);
+        }catch(DataLayerException dte){
+            request.setAttribute("message", "Data access exception: " + dte.getMessage());
+            action_error(request, response);
+        }  
+    }
+    
+    private void action_show_home_student(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException, TemplateManagerException {
+        try{
+            TemplateResult res = new TemplateResult(getServletContext());
+            request.setAttribute("page_title", "Riepilogo Tirocini e Candidature");
+            HttpSession s = SecurityLayer.checkSession(request);
+            int userid = (int)s.getAttribute("userid");
+            //ottengo dal db le richieste e i tirocini relativi allo studente
+            List<Richiesta> lista_richieste = ((InternShipDataLayer)request.getAttribute("datalayer")).getListaRichiesteStudente(userid);
+            List<Tirocinio> lista_tirocini_approvati = ((InternShipDataLayer)request.getAttribute("datalayer")).getListaTirociniApprovatiByStudente(userid);
+            if(!lista_richieste.isEmpty()){
+                request.setAttribute("lista_richieste", lista_richieste);
+            }
+            if(!lista_tirocini_approvati.isEmpty()){
+                request.setAttribute("lista_tirocini_approvati", lista_tirocini_approvati);
+            }
+            request.setAttribute("Session", s);
+            request.setAttribute("strip_slashes", new SplitSlashesFmkExt());
+            res.activate("home.ftl.html", request, response);
+        }catch(DataLayerException dte){
+            request.setAttribute("message", "Data access exception: " + dte.getMessage());
+            action_error(request, response);
+        }  
+    }
+    
+    private void action_show_home_company(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException, TemplateManagerException {
+        try{
+            TemplateResult res = new TemplateResult(getServletContext());
+            request.setAttribute("page_title", "Gestione Tirocini Inseriti");
+            HttpSession s = SecurityLayer.checkSession(request);
+            int userid = (int)s.getAttribute("userid");
+            Azienda azienda = ((InternShipDataLayer)request.getAttribute("datalayer")).getInfoAzienda(userid);
+            List<Tirocinio> lista_tirocini = ((InternShipDataLayer)request.getAttribute("datalayer")).getListaTirociniByAzienda(azienda);
+            if(!lista_tirocini.isEmpty()){
+                request.setAttribute("lista_tirocini", lista_tirocini);
+                Map resoconto = new HashMap();
+                for (int i = 0; i < lista_tirocini.size(); i++) {	
+                    Date dataFine = lista_tirocini.get(i).getDataFine();
+                    if(dataFine!=null){
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                        Date dateNow = new Date();
+                        if(dateNow.compareTo(dataFine)>0){
+                            resoconto.put(lista_tirocini.get(i).getIdTirocinio(), true);
+                        }else{
+                            resoconto.put(lista_tirocini.get(i).getIdTirocinio(), false);
+                        }
+                    }
+                }
+                request.setAttribute("resoconto", resoconto);
+            }      
+            request.setAttribute("strip_slashes", new SplitSlashesFmkExt());
+            request.setAttribute("Session", s);
+            res.activate("home.ftl.html", request, response);
+        }catch(DataLayerException dte){
+            request.setAttribute("message", "Data access exception: " + dte.getMessage());
+            action_error(request, response);
+        }  
+    }
+    
     private void action_error(HttpServletRequest request, HttpServletResponse response){
         if (request.getAttribute("exception")!=null) {
             (new FailureResult(getServletContext())).activate((Exception) request.getAttribute("exception"), request, response);
@@ -55,9 +136,24 @@ public class Homepage extends InternshipDBController {
     
     @Override
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)throws ServletException{
-        request.setAttribute("page_title", "Homepage");
-        try{         
-            action_show_home(request, response);          
+        try{
+            HttpSession s = SecurityLayer.checkSession(request);
+            if(s!=null){
+                String type = (String)s.getAttribute("type");
+                if(type.equals("stud")){
+                    //sei studente
+                    action_show_home_student(request, response);
+                }else if(type.equals("comp")){
+                    //sei un'azienda
+                    action_show_home_company(request, response);
+                }else if(type.equals("admin")){
+                    //sei admin
+                    action_show_home_admin(request, response);
+                }
+            }else{
+                //sei anonimo
+                action_show_home_anonimo(request, response);
+            }
         }catch(IOException ex){
             request.setAttribute("exception", ex);
             action_error(request, response);
